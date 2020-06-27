6aa0431a567821033465138c16d1874f073ea0ed
MuhammadDaffianoRahmatullah <62738880+MuhammadDaffianoRahmatullah@users.noreply.github.com>
2020-06-27 00:17:59 -0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="POST" action="welcome.php">
+		<pre>
+<center><h1>Form Pengisian KTP</h1></center>
+NIK :                      <input type="text" name="nik"><br>
+Nama :                     <input type="text" name="nama"><br>
+Tempat, Tanggal Lahir :    <input type="text" name="ttl"><br>
+Jenis Kelamin :            <input type="checkbox" name="jk">Laki-laki <input type="checkbox" name="jk">Wanita<br>
+Alamat :                   <textarea name="alamat">
+      
+
+
+                       </textarea><br>
+Rt/Rw :                    <input type="text" name="rtw"><br>
+Kel/Desa :                 <input type="text" name="keldes"><br>
+Kecamatan :                <input type="text" name="kec"><br>
+Agama :                    <select style="width: 200;" name="agama">
+      <option></option>
+      <option>Islam</option>
+    <option>Kristen</option>
+    <option>Khatolik</option>
+    <option>Hindu</option>
+    <option>Budha</option>
+    <option>Konghucu</option>
+    </select><br>
+Status Perkawinan :        <input type="checkbox" name="sp">Kawin <input type="checkbox" name="sp">Belum Kawin<br>
+Pekerjaan :                <input type="text" name="job"><br>
+Kewarganegaraan :          <input type="text" name="warga"><br>
+Berlaku Hingga :           <input type="text" name="laku"><br>
+Tanda Tangan :             <textarea name="ttd">
+      
+
+
+                       </textarea><br>
+<center><input type="submit"></pre></center>
+	</form>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<pre>
+	<center><h1>Data KTP Anda</h1></center><br><br>
+NIK : <?php echo $_POST["nik"]; ?><br>
+Nama : <?php echo $_POST["nama"]; ?><br>
+Tempat, Tanggal Lahir : <?php echo $_POST["ttl"]; ?><br>
+Jenis Kelamin : <?php echo $_POST["jk"]; ?><br>
+Alamat : <?php echo $_POST["alamat"]; ?><br>
+Rt/Rw : <?php echo $_POST["rtw"]; ?><br>
+Kel/Desa : <?php echo $_POST["keldes"]; ?><br>
+Kecamatan : <?php echo $_POST["kec"]; ?><br>
+Agama : <?php echo $_POST["agama"]; ?><br>
+Status Perkawinan : <?php echo $_POST["sp"]; ?><br>
+Pekerjaan : <?php echo $_POST["job"]; ?><br>
+Kewarganegaraan : <?php echo $_POST["warga"]; ?><br>
+Berlaku Hingga : <?php echo $_POST["laku"]; ?><br>
+</pre>
+</body>
+</html>
